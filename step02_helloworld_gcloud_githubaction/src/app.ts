775656922b4dbe1a16142aeac3508d2ce4465357
zeeshanhanif <zeeshanhanif@gmail.com>
2020-06-26 20:59:54 +0500

    Step 02 updated github action

--- step02_helloworld_gcloud_githubaction/src/app.ts
+++ step02_helloworld_gcloud_githubaction/src/app.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || "8080";
 
 app.get('/', (req, res) => {
-  res.send('Hello from Zeeshan! Google Cloud Run with Github Action');
+  res.send('Hello from Zeeshan! Google Cloud Run with Github Action!');
 });
 app.listen(port, (err) => {
   if (err) {
